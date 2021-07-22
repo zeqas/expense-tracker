a4a66094c0ad6cb547df92924fecb65bae44d1bd
zeqas <casperi52400@gmail.com>
2021-07-22 17:58:31 +0800

    feat: add date-to-string and input-validation functions

---
+++ tools/dateToString.js
@@ -0,0 +1,9 @@
+function dateToString(date) {
+  const day = ('0' + String(date.getDate())).slice(-2)
+  const month = ('0' + String(date.getMonth() + 1)).slice(-2)
+  const year = String(date.getFullYear())
+  const dateArray = [year, month, day]
+  return dateArray.join('-')
+}
+
+module.export = dateToString
---
+++ tools/inputValidation.js
@@ -0,0 +1,17 @@
+function inputValidation(data) {
+  const { name, category, date, amount } = data
+  const validationResults = {
+    name: true,
+    category: true,
+    date: true,
+    amount: true
+  }
+  name.trim().length === 0 ? validationResults.name = false : ''
+  category.length === 0 ? validationResults.category = false : ''
+  date.length === 0 ? validationResults.date = false : ''
+  amount.length === 0 ? validationResults.amount = false : ''
+
+  return validationResults
+}
+
+module.exports = inputValidation
