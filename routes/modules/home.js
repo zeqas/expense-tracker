f4d33e46d4a378ff73fefc6d79e9ac08cb530a3a
zeqas <casperi52400@gmail.com>
2021-07-28 23:15:34 +0800

    refactor: rewirte the route and add some notes

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,35 +1,61 @@
 const express = require('express')
 const router = express.Router()
 
-const Record = require('../../models/record')
 const Category = require('../../models/category')
+const Record = require('../../models/record')
+
+const dateToString = require('../../tools/dateToString')
+
 
 router.get('/', async (req, res) => {
-  const category = req.query.category
-  const filter = {}
-  
-  // 假設 category 存在
-  // filter 陣列放入 req.query.category
+  const categoryList = await Category.find().lean()
+  const categoryData = {}
 
-  if (category) { filter.category = category }
+  categoryList.forEach(category => {
+    // 舉例： '交通出行'　替換成　'fas fa-shuttle-van'，並存放到categoryData內
+    categoryData[category.categoryName] = category.categoryIcon
+  })
 
-  // async & await
-  // 等待 Category 執行完畢，確保categories 包含所有資料
-  const categories = await Category.find().lean()
-
-  Record.find(filter)
-    .populate('category') // 引用文檔
+  return Record.find()
+    .sort({ date: 'asc' })
     .lean()
     .then(records => {
-      // // 轉換日期為字串
-
-      // records.forEach(record => record.date = dateToString(record.date))
-      
       let totalAmount = 0
-      records.forEach(record => totalAmount += record.amount)
-      res.render('index', { categories, category, records, totalAmount })
+      // console.log(records)
+      records.map(record => {
+        totalAmount += record.amount
+        record.date = dateToString(record.date)
+        record.categoryIcon = categoryData[record.category]
+      })
+      res.render('index', { records, totalAmount, categoryList })
     })
     .catch(err => console.error(err))
+})
+
+router.get('/filter', async (req, res) => {
+  const categoryName = req.query.category
+  const categoryList = await Category.find().lean()
+
+  // 只列出 Category 內符合categoryName的檔案
+  const category = await Category.findOne({ categoryName })
+  
+  if (!category) return res.redirect('/')
+
+  // 列出所有在 Record 內符合的 record(可能要加s) #沒開黃腔
+  // 應該可以不用另外設置 filter 就篩選出類別？
+  return Record.find({ category: category.categoryName })
+    .sort({ date: 'asc' })
+    .lean()
+    .then(records => {
+      let totalAmount = 0
+      records.map(record => {
+        record.date = dateToString(record.date)
+        totalAmount += record.amount
+        record.categoryIcon = category.categoryIcon
+      })
+      res.render('index', { records, totalAmount, theCategory: category.categoryName, categoryList })
+    })
+
 })
 
 module.exports = router
