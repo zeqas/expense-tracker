25ee27e3391936dad227a0cbd19b6441e12c95a8
zeqas <casperi52400@gmail.com>
2021-07-30 17:38:32 +0800

    feat: rewrite and fix bugs in routes and views

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-
+const mongoose = require('mongoose')
 const Category = require('../../models/category')
 const Record = require('../../models/record')
 
 const dateToString = require('../../tools/dateToString')
+const inputValidation = require('../../tools/inputValidation')
 
 // 目前時間 = today
 
@@ -13,46 +14,40 @@ today = dateToString(today)
 
 // create
 router.get('/new', async (req, res) => {
-  const categories = await Category.find().lean()
-  res.render('new', { today, categories })
+  // 取得類別資料
+  const categoryList = await Category.find().lean()
+  res.render('new', { today, categoryList })
 })
 
-router.post('/', (req, res) => {
-  Record.create(req.body)
+router.post('/new', (req, res) => {
+  const record = req.body
+  console.log(record)
+  const validation = inputValidation(record)
+  // 驗證功能
+  // 如何處理在顯示錯誤後，再次選擇類別?
+  if (Object.values(validation).includes(false)) {
+    res.render('new', { validation, today, record })
+  } else {
+    return Record.create({
+      name: record.name,
+      category: record.category,
+      date: record.date,
+      amount: record.amount
+    })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
+  }
 })
-
-// 如果要驗證表單?
-
-// router.post('/new', (req, res) => {
-//   const record = req.body
-//   const validation = inputValidation(record)
-
-//   if (Object.values(validation).includes(false)) {
-//     res.render('new', { validation, today, record })
-//   } else {
-//     return Record.create({
-//       name: record.name,
-//       date: record.date,
-//       category: record.category,
-//       amount: record.amount
-//     })
-//       .then(() => res.redirect('/'))
-//       .catch(err => console.error(err))
-//   }
-// })
-
 
 // edit page
 router.get('/:id/edit', async (req, res) => {
   const id = req.params.id
-  const categories = await Category.find().lean()
+  const categoryList = await Category.find().lean()
   return Record.findById(id)
     .lean()
     .then(record => {
       const currentDate = dateToString(record.date) 
-      res.render('edit', { record, currentDate, categories })
+      res.render('edit', { record, currentDate, categoryList })
     })
     .catch(error => console.log(error))
 })
@@ -60,43 +55,40 @@ router.get('/:id/edit', async (req, res) => {
 // edit record
 router.put('/:id', (req, res) => {
   const id = req.params.id
-  return Record.findById(id)
-    .then(record => {
-      // 合併 record 和 req.body
-      Object.assign(record, req.body)
-      return record.save()
-    })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+  const editedRecord = req.body
+  const validation = inputValidation(editedRecord)
 
-  
-  // const updatedRecord = req.body
-  // const validation = inputValidation(updatedRecord) 
-  // if (Object.values(validation).includes(false)) {
-  //   return Record.findById(id)
-  //     .lean()
-  //     .then(record => {
-  //       const currentDate = dateToString(record.date)
-  //       res.render('edit', { record, currentDate, validation })
-  //     })
-  //     .catch(err => console.error(err))
-  // } else {
-  //   return Record.findById(id)
-  //     .then(record => {
-  //       record.name = updatedRecord.name
-  //       record.category = updatedRecord.category
-  //       record.date = updatedRecord.date
-  //       record.amount = updatedRecord.amount
-  //       return record.save()
-  //     })
-  //     .then(() => res.redirect(`/records/${id}`))
-  //     .catch(error => console.error(error))
-  // }
+  if (Object.values(validation).includes(false)) {
+    return Record.findById(id)
+      .lean()
+      .then(record => {
+        const currentDate = dateToString(record.date)
+        res.render('edit', { record, currentDate, validation })
+      })
+      .catch(err => console.error(err))
+  } else {
+    return Record.findById(id)
+      .then(record => {
+        [ record.name, record.category, record.date, record.amount ] = [ editedRecord.name, 
+        editedRecord.category, 
+        editedRecord.date, 
+        editedRecord.amount ]
+        return record.save()
+      })
+      .then(() => {
+        res.redirect('/')
+      })
+      .catch(err => console.error(err))
+  }
 })
 
 // delete
 router.delete('/:id', (req, res) => {
   const id = req.params.id
+  // id 不存在則返回首頁
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.redirect('/')
+  }
   return Record.findById(id)
     .then(record => record.remove())
     .then(() => res.redirect('/'))
